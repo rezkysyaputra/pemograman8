afd88acd9c3edfc5caed9d217588e99b596a3140
Rezky Syaputra <muhrezkysyaputra100@gmail.com>
2023-06-02 15:40:19 +0700

    add constructor

---
+++ pemrog8/pemrog8/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace pemrog8
+{
+    // Class Person sebagai Class Utama
+    public class Person
+    {
+        // Properties/field dari class Person
+        string Name;
+        int Age;
+
+        // constructor dari class person
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        // fungsi/method yang menampilkan nama dan umur
+        public void InfoPerson()
+        {
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun", Name, Age);
+        }
+    }
+
+    // class Karyawan berupa class turunan dari class Person
+    public class Karyawan : Person
+    {
+
+        // properties/field dari class Karyawan
+        public string karyawanid;
+        public string subject;
+
+        // constructor dari class Person yang dimana mengambil field dari class Person
+        public Karyawan(string kid, string sbj, string name, int age) : base(name, age)
+        {
+            karyawanid = kid;
+            subject = sbj;
+        }
+
+        // method dari class Karyawan
+        public void InfoKaryawan()
+        {
+
+            Console.WriteLine("Posisi saya sebagai {0} dengan id {1}", subject, karyawanid);
+        }
+
+        // Override sebuah method dari class Person
+        public void InfoPerson(string name, int age)
+        {
+
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun", name, age);
+        }
+
+    }
+}
---
+++ pemrog8/pemrog8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace pemrog8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<Class utama>");
+            // Object dari class Person dengan memakai constructor
+            Person person1 = new Person("Rahmat budiman", 19);
+            // Memanggil method dari class Person
+            person1.InfoPerson();
+
+            Console.WriteLine("================================");
+
+            Console.WriteLine("<Class turunan>");
+            // Object dari class Karyawan dengan memakai constructor
+            Karyawan karyawan1 = new Karyawan("11223344", "Manager", "Asep samsudin", 22);
+            // Memanggil method yang sudah di override dari class Person ke class Karyawan
+            karyawan1.InfoPerson();
+            // Memanggil method dari class Karyawan
+            karyawan1.InfoKaryawan();
+            
+            Console.ReadKey();
+        }
+
+    }
+}
